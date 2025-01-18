c2193d997c7b143eba945611bf32cf2bcf32da74
Chaitanya Wankhede <chaitanyawankhede705@gmail.com>
2025-01-18 10:03:32 +0530

    init user views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('dashboard')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def dashboard(request):
+    user = request.user
+    if user.role == 'admin':
+        return render(request, 'users/admin_dashboard.html')
+    elif user.role == 'developer':
+        return render(request, 'users/developer_dashboard.html')
+    else:  # tester
+        return render(request, 'users/tester_dashboard.html')
