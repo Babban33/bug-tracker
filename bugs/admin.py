cfffb1f0c184115b860c2a24bb3399af96e95cb6
Chaitanya Wankhede <chaitanyawankhede705@gmail.com>
2025-01-18 20:47:05 +0530

    added admin on Bugs

--- bugs/admin.py
+++ bugs/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Bug
 
-# Register your models here.
+@admin.register(Bug)
+class BugAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'priority', 'assigned_to', 'created_at')
+    search_fields = ('title', 'description')
